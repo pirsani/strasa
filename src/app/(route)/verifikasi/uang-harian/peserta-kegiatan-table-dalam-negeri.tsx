1f7bf25a874d2c20b62c6ad1ba947174c462bb95
devfh <devfh@pirsani.id>
2024-10-10 02:33:32 +0200

    sorting dan pagination untuk tabel peserta UH dalam negeri

--- src/app/(route)/verifikasi/uang-harian/peserta-kegiatan-table-dalam-negeri.tsx
+++ src/app/(route)/verifikasi/uang-harian/peserta-kegiatan-table-dalam-negeri.tsx
@@ -1,14 +1,23 @@
 import { PesertaKegiatanDalamNegeri } from "@/actions/kegiatan/peserta/dalam-negeri";
 import { TableCellInput } from "@/components/datatable/table-cell-input";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  Table,
   useReactTable,
 } from "@tanstack/react-table";
-import { useEffect, useState } from "react";
+import { ArrowDownUp, ChevronLeft, ChevronRight } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
+interface RowData {
+  [key: string]: any; // Replace with actual field names and types if known
+}
 // Define column structure
 const columns: ColumnDef<PesertaKegiatanDalamNegeri>[] = [
   {
@@ -23,7 +32,7 @@ const columns: ColumnDef<PesertaKegiatanDalamNegeri>[] = [
   },
   {
     accessorKey: "pangkatGolonganId",
-    header: "Pangkat/Golongan",
+    header: "Pangkat/ Golongan",
     cell: (info) => info.getValue(),
   },
   {
@@ -36,29 +45,29 @@ const columns: ColumnDef<PesertaKegiatanDalamNegeri>[] = [
     header: "Eselon",
     cell: (info) => info.getValue(),
   },
-  {
-    accessorKey: "email",
-    header: "Email",
-    cell: (info) => info.getValue(),
-  },
-  {
-    accessorKey: "bank",
-    header: "Bank",
-    cell: (info) => info.getValue(),
-  },
-  {
-    accessorKey: "namaRekening",
-    header: "Nama Rekening",
-    cell: (info) => info.getValue(),
-  },
-  {
-    accessorKey: "nomorRekening",
-    header: "Nomor Rekening",
-    cell: (info) => info.getValue(),
-  },
+  // {
+  //   accessorKey: "email",
+  //   header: "Email",
+  //   cell: (info) => info.getValue(),
+  // },
+  // {
+  //   accessorKey: "bank",
+  //   header: "Bank",
+  //   cell: (info) => info.getValue(),
+  // },
+  // {
+  //   accessorKey: "namaRekening",
+  //   header: "Nama Rekening",
+  //   cell: (info) => info.getValue(),
+  // },
+  // {
+  //   accessorKey: "nomorRekening",
+  //   header: "Nomor Rekening",
+  //   cell: (info) => info.getValue(),
+  // },
   {
     accessorKey: "uhDalamNegeri.hFullboard",
-    header: "(N) Fullboard",
+    header: "Hari Fullboard",
     cell: ({ getValue, row, column, table }) =>
       TableCellInput({
         getValue,
@@ -74,7 +83,7 @@ const columns: ColumnDef<PesertaKegiatanDalamNegeri>[] = [
   },
   {
     accessorKey: "uhDalamNegeri.hFulldayHalfday",
-    header: "(N) Fullday/ Halfday",
+    header: "Hari Fullday/ Halfday",
     cell: ({ getValue, row, column, table }) =>
       TableCellInput({
         getValue,
@@ -90,7 +99,7 @@ const columns: ColumnDef<PesertaKegiatanDalamNegeri>[] = [
   },
   {
     accessorKey: "uhDalamNegeri.hDalamKota",
-    header: "(N) Dalam Kota",
+    header: "Hari Dalam Kota",
     cell: ({ getValue, row, column, table }) =>
       TableCellInput({
         getValue,
@@ -107,7 +116,7 @@ const columns: ColumnDef<PesertaKegiatanDalamNegeri>[] = [
 
   {
     accessorKey: "uhDalamNegeri.hLuarKota",
-    header: "(N) Luar Kota",
+    header: "Hari Luar Kota",
     cell: ({ getValue, row, column, table }) =>
       TableCellInput({
         getValue,
@@ -123,7 +132,7 @@ const columns: ColumnDef<PesertaKegiatanDalamNegeri>[] = [
   },
   {
     accessorKey: "uhDalamNegeri.hDiklat",
-    header: "(N) Diklat",
+    header: "Hari Diklat",
     cell: ({ getValue, row, column, table }) =>
       TableCellInput({
         getValue,
@@ -139,7 +148,7 @@ const columns: ColumnDef<PesertaKegiatanDalamNegeri>[] = [
   },
   {
     accessorKey: "uhDalamNegeri.uhTransport",
-    header: "(N) Transport",
+    header: "Hari Transport",
     cell: ({ getValue, row, column, table }) =>
       TableCellInput({
         getValue,
@@ -167,40 +176,40 @@ export const PesertaKegiatanTable = ({
   data: defaultData,
 }: PesertaKegiatanTableProps) => {
   const [data, setData] = useState(() => [...defaultData]);
+  const [pageSize, setPageSize] = useState(10); // Set the initial page size
+  const [pageIndex, setPageIndex] = useState(0); // Set the initial page index
+  const [sorting, setSorting] = useState<SortingState>([]); // Explicitly define SortingState
+  const [cumulativeWidths, setCumulativeWidths] = useState<number[]>([]);
+  const colRefs = useRef<HTMLTableCellElement[]>([]);
+  const frozenColumnCount = 1;
 
   useEffect(() => {
     setData(defaultData);
   }, [defaultData]);
 
-  const calculateTotal = (
-    row: PesertaKegiatanDalamNegeri
-  ): PesertaKegiatanDalamNegeri => {
-    if (!row.uhDalamNegeri) {
-      return row;
-    }
-    const fullboard = row.uhDalamNegeri?.hFullboard || 0;
-    const fulldayHalfday = row.uhDalamNegeri?.hFulldayHalfday || 0;
-    const dalamKota = row.uhDalamNegeri?.hDalamKota || 0;
-    const luarKota = row.uhDalamNegeri?.hLuarKota || 0;
-    const diklat = row.uhDalamNegeri?.hDiklat || 0;
-    const transport = row.uhDalamNegeri?.uhTransport || 0;
-
-    const total: number =
-      fullboard + fulldayHalfday + dalamKota + luarKota + diklat + transport;
-
-    return {
-      ...row,
-      uhDalamNegeri: {
-        ...row.uhDalamNegeri,
-        jumlahHari: total,
-      },
-    };
-  };
-
   const table = useReactTable({
     data,
     columns,
+    pageCount: Math.ceil(data.length / pageSize), // Dynamic page count
+    state: {
+      pagination: {
+        pageIndex,
+        pageSize,
+      },
+      sorting, // Add sorting state to the table
+    },
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(), // Sorting model
+    onSortingChange: setSorting, // Handle sorting state changes
+    onPaginationChange: (updater) => {
+      const nextPagination =
+        typeof updater === "function"
+          ? updater({ pageIndex, pageSize })
+          : updater;
+      setPageIndex(nextPagination.pageIndex);
+      setPageSize(nextPagination.pageSize);
+    },
     meta: {
       updateData: (rowIndex: number, columnId: string, value: string) => {
         setData((old) =>
@@ -250,48 +259,280 @@ export const PesertaKegiatanTable = ({
     },
   });
 
+  useEffect(() => {
+    if (
+      frozenColumnCount > 0 &&
+      colRefs.current &&
+      colRefs.current.length >= frozenColumnCount
+    ) {
+      const colWidths = colRefs.current.map(
+        (col) => col.getBoundingClientRect().width
+      );
+      const cumulativeWidths = colWidths.reduce(
+        (acc, width) => [...acc, acc[acc.length - 1] + width],
+        [0]
+      );
+      setCumulativeWidths(cumulativeWidths);
+      //console.log("cumulativeWidths", cumulativeWidths);
+    }
+  }, [frozenColumnCount, colRefs]); // Only run when frozen column count or refs change
+  //[table.getRowModel().rows]); // Recalculate when rows change
+
   return (
-    <div className="overflow-x-auto w-full">
-      <table className="min-w-full table-auto border-collapse border border-gray-300">
-        <thead className="bg-gray-100">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header, index) => (
-                <th
-                  key={header.id}
-                  className={`p-2 border border-gray-300 w-12 ${
-                    index < 1 ? "sticky left-0 bg-white z-10" : ""
-                  } ${index === 1 ? "left-40" : "left-0"}`} // Adjust based on column width
-                >
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id} className="even:bg-gray-50">
-              {row.getVisibleCells().map((cell, index) => (
-                <td
-                  key={cell.id}
-                  className={cn(
-                    `p-2 border border-gray-300 ${
-                      index < 1 ? "sticky left-0 bg-white z-10" : ""
-                    } ${index === 1 ? "left-40" : "left-0"}`,
-                    cell.column.columnDef.meta?.className || ""
-                  )} // Adjust based on column width
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <div className="overflow-x-auto w-full pb-6">
+        <table className="min-w-full table-auto border-collapse border border-gray-300">
+          <thead>
+            {table.getHeaderGroups().map((headerGroup, index) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header, index) => {
+                  const columnRelativeDepth =
+                    header.depth - header.column.depth;
+                  if (columnRelativeDepth > 1) {
+                    return null;
+                  }
+
+                  let rowSpan = 1;
+                  if (header.isPlaceholder) {
+                    const leafs = header.getLeafHeaders();
+                    rowSpan = leafs[leafs.length - 1].depth - header.depth;
+                  }
+
+                  const isGroupHeader =
+                    header.subHeaders && header.subHeaders.length > 1;
+
+                  return (
+                    <th
+                      key={header.id}
+                      ref={(el) => {
+                        if (
+                          index < frozenColumnCount &&
+                          el instanceof HTMLTableCellElement
+                        ) {
+                          colRefs.current[index] = el;
+                        }
+                      }}
+                      colSpan={header.colSpan}
+                      rowSpan={rowSpan}
+                      className={cn(
+                        "px-2 border border-gray-300",
+                        !isGroupHeader
+                          ? "hover:cursor-pointer bg-gray-50"
+                          : "bg-gray-100",
+
+                        {
+                          [`sticky left-0 z-${10 - index}`]:
+                            index < frozenColumnCount, // Sticky columns
+                          "left-0": index >= frozenColumnCount, // Default position for other columns
+                        },
+                        header.column.columnDef.meta?.className
+                      )}
+                      style={
+                        index < frozenColumnCount
+                          ? { left: `${cumulativeWidths[index] || 0}px` }
+                          : undefined
+                      }
+                    >
+                      <div
+                        className={cn(
+                          "flex flex-row items-center w-full h-full gap-2"
+                        )}
+                      >
+                        {/* {header.isPlaceholder ? "y" : "n"}
+                        {header.column.columnDef.header ? "y" : "n"} */}
+                        <span className="flex-grow">
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                        </span>
+                        {/* Sorting icon only for non-grouped columns */}
+                        {!isGroupHeader && header.column.getCanSort() && (
+                          <span
+                            className="hover:cursor-pointer"
+                            onClick={header.column.getToggleSortingHandler()} // Enable sorting on click
+                          >
+                            {(() => {
+                              const sortOrder = header.column.getIsSorted();
+                              if (sortOrder === false) {
+                                return <ArrowDownUp size={16} />; // Not sorted icon
+                              }
+                              return sortOrder === "asc" ? "🔼" : "🔽"; // Sort ascending/descending icon
+                            })()}
+                          </span>
+                        )}
+                        {/* {isGroupHeader && (
+                          <span className="hover:cursor-pointer">
+                            {header.subHeaders.length}
+                          </span>
+                        )} */}
+                      </div>
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row, rowIndex) => (
+              <tr key={row.id} className="odd:bg-white even:bg-gray-50">
+                {row.getVisibleCells().map((cell, index) => {
+                  const field = cell.column.columnDef.meta?.field;
+                  //This will ensure TypeScript understands that row.original can be indexed by a string.
+                  const fieldValue = field
+                    ? (row.original as RowData)[field]
+                    : undefined;
+                  return (
+                    <td
+                      key={cell.id}
+                      ref={(el) => {
+                        if (
+                          index < frozenColumnCount &&
+                          el instanceof HTMLTableCellElement
+                        ) {
+                          colRefs.current[index] = el;
+                        }
+                      }}
+                      className={cn(
+                        "px-2 border border-gray-300 ",
+                        {
+                          [`sticky left-0 bg-gray-100 z-${10 - index}`]:
+                            index < frozenColumnCount, // Sticky columns
+                          "left-0": index >= frozenColumnCount, // Default position for other columns
+                        },
+                        cell.column.columnDef.meta?.className
+                      )}
+                      style={
+                        index < frozenColumnCount
+                          ? { left: `${cumulativeWidths[index] || 0}px` }
+                          : undefined
+                      }
+                    >
+                      {(() => {
+                        if (cell.column.id === "rowNumber") {
+                          // Display the row number, considering pagination
+                          return rowIndex + 1 + pageSize * pageIndex;
+                        }
+
+                        if (cell.column.id === "_additionalKolomAksi") {
+                          // Always render the "aksi" column with flexRender
+
+                          return flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          );
+                        }
+
+                        // Display the cell value if not in edit mode
+                        return flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        );
+                      })()}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <PaginationControls table={table} />
     </div>
   );
 };
+
+interface PaginationControlsProps<T> {
+  table: Table<T>;
+}
+
+export const PaginationControls = <T,>({
+  table,
+}: PaginationControlsProps<T>) => {
+  const [jumpPage, setJumpPage] = useState(
+    table.getState().pagination.pageIndex + 1
+  );
+  const pageCount = table.getPageCount();
+
+  const handlePageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const page = Number(e.target.value);
+    if (page >= 1 && page <= table.getPageCount()) {
+      table.setPageIndex(page - 1); // pageIndex is zero-based
+    }
+  };
+
+  return (
+    <div className="my-2 flex flex-row gapx-2 sm:gap-2 items-center">
+      {/* Pagination controls */}
+      <Button
+        variant={"outline"}
+        onClick={() => table.previousPage()}
+        disabled={!table.getCanPreviousPage()}
+        className="p-2"
+      >
+        <ChevronLeft size={24} />
+        <span className="hidden sm:block">Previous</span>
+      </Button>
+
+      <select
+        value={table.getState().pagination.pageIndex + 1}
+        onChange={handlePageChange}
+        className="mx-2 p-2 rounded-sm border border-gray-300"
+      >
+        {Array.from({ length: pageCount }, (_, i) => (
+          <option key={i} value={i + 1}>
+            Page {i + 1} of {pageCount}
+          </option>
+        ))}
+      </select>
+
+      <Button
+        variant={"outline"}
+        onClick={() => table.nextPage()}
+        disabled={!table.getCanNextPage()}
+        className="p-2"
+      >
+        <ChevronRight size={24} />
+        <span className="hidden sm:block">Next</span>
+      </Button>
+
+      {/* Select page size */}
+      <select
+        value={table.getState().pagination.pageSize}
+        onChange={(e) => table.setPageSize(Number(e.target.value))}
+        className="mx-2 p-2 rounded-sm border border-gray-300"
+      >
+        {[5, 10, 20, 30, 40, 50].map((pageSize) => (
+          <option key={pageSize} value={pageSize}>
+            Show {pageSize}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+function calculateTotal(
+  row: PesertaKegiatanDalamNegeri
+): PesertaKegiatanDalamNegeri {
+  if (!row.uhDalamNegeri) {
+    return row;
+  }
+  const fullboard = row.uhDalamNegeri?.hFullboard || 0;
+  const fulldayHalfday = row.uhDalamNegeri?.hFulldayHalfday || 0;
+  const dalamKota = row.uhDalamNegeri?.hDalamKota || 0;
+  const luarKota = row.uhDalamNegeri?.hLuarKota || 0;
+  const diklat = row.uhDalamNegeri?.hDiklat || 0;
+  const transport = row.uhDalamNegeri?.uhTransport || 0;
+
+  const total: number =
+    fullboard + fulldayHalfday + dalamKota + luarKota + diklat + transport;
+
+  return {
+    ...row,
+    uhDalamNegeri: {
+      ...row.uhDalamNegeri,
+      jumlahHari: total,
+    },
+  };
+}
